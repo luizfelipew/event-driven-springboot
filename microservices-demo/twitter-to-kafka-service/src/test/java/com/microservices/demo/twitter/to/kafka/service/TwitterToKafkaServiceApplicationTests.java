182b0a0309808fcbc51878dbc94e36908b79cba6
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-07-31 21:52:29 -0300

    add: :art: adding first microservice

---
+++ microservices-demo/twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTests {
+
+//    @Test
+//    void contextLoad() {
+//
+//    }
+}
